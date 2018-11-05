19900e051698854fd61dce4e0ec5658e21832e3a
kirschkernchen <43247980+kirschkernchen@users.noreply.github.com>
2018-11-05 12:24:46 +0000

    better data management

--- Angle function.py
+++ Angle function.py
@@ -1,5 +1,7 @@
+import numpy as np
+import pandas as pd
 
-def calculate_angle (nose_x, nose_y, tail_x, tail_y):
+def calculate_angle (coord_df, female=TRUE):
     """
     Function to calculate the angle of the mouse in relation to the space.
     The angle is calculated from the vector from the nose coordinate and tail coordinate
@@ -7,20 +9,23 @@ def calculate_angle (nose_x, nose_y, tail_x, tail_y):
     :param coordinates for nose an tail in each their own numpy array
     :return Angle of animal
     """
+ length = len(coord_df['female_nose_x'])
+ size=np.zeros(length)
+ angles_df = pd.DataFrame({"female":
+                          , "male":})
+
 ### load male data points
+### load female data points
 ### calculate nose-tail-distance for x and y
 ### write angles column 1 (= male)
 
-###
+if female == TRUE:
+    delta_x = coord_df['female_nose_x']-coord_df['female_tail_x']
+    delta_y = coord_df['female_nose_y']-coord_df['female_tail_y']
 
-n_x = nose_x
-n_y = nose_y
-t_x = tail_x
-t_y = tail_y
+    angle = np.arctan (delta_y/delta_x)
 
-nose_tail_coor_df = pd.DataFrame({"nose_x": n_x[:-1],
-                                  "direction": direction,
-                                  "time": time})
+if female == FALSE:
 
 
 delta_x = n_x - t_x
