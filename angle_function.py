7cd34412ffcbc74b2698b0b707b64d0120460ec7
Sarah <saraholesen@hotmail.com>
2018-11-05 15:39:24 +0000

    Finished function

--- angle_function.py
+++ angle_function.py
@@ -8,9 +8,9 @@ import pickle
 #tail_y = [20, 40]
 
 #test_df = pd.DataFrame({"female_nose_x":nose_x,
-                        "female_tail_x":tail_x,
-                        "female_nose_y":nose_y,
-                        'female_tail_y':tail_y})
+                       # "female_tail_x":tail_x,
+                       # "female_nose_y":nose_y,
+                       # 'female_tail_y':tail_y})
 
 def calculate_angle (coord_df, female=True, male=True):
     """
@@ -18,16 +18,11 @@ def calculate_angle (coord_df, female=True, male=True):
     The angle is calculated from the vector from the nose coordinate and tail coordinate
 
     :param coordinates for nose an tail in each their own numpy array
-    :return Angle of animal
+    :return pd.Dataframe containing the angle of male and female mice
     """
     column_names = ['female', 'male']
     zero_df = np.zeros(shape=(len(coord_df),len(column_names)))
     angles_df = pd.DataFrame(zero_df, columns=column_names)
-
-### load male data points
-### load female data points
-### calculate nose-tail-distance for x and y
-### write angles column 1 (= male)
 
     if female == True:
         delta_x = coord_df['female_nose_x']-coord_df['female_tail_x']
@@ -40,6 +35,10 @@ def calculate_angle (coord_df, female=True, male=True):
         delta_y = coord_df['male_nose_y'] - coord_df['male_tail_y']
 
         angles_df['female'] = np.arctan(delta_y / delta_x)
+
+    # save as pickle file in data folder
+    with open("data/angles_df" + ".pkl", "wb") as f:
+        pickle.dump(angles_df, f)
 
     return angles_df
 
