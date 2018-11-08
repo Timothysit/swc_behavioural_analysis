b8081f215cf7eee1fe8058c5007043454f48fccf
kirschkernchen <43247980+kirschkernchen@users.noreply.github.com>
2018-11-08 10:10:16 +0000

    Manual Annotation

--- manual_annotation.py
+++ manual_annotation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 ### read in manual annotation files
-man_annot_df = pd.read_csv('data\Annot_Manual_vs3.csv', sep=',')
+man_annot_df = pd.read_csv('annot_manual_final.csv', sep=',')
 man_stats_df = pd.read_csv('data\Annot_Manual_vs3_STATS.csv', sep=',')
 
